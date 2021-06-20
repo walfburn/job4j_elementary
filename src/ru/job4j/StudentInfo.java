0946805afdbc6b19983f6ef6b1d5159b143c1244
Vladimir <walfburner@mail.ru>
2021-06-20 17:37:13 +0300

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vladimir");
+        System.out.println("11.06.1994");
+    }
+}
