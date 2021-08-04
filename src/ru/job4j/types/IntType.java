4f6d67a83aedd0b4db20e790fad3bc7196ed300e
Vladimir <walfburner@mail.ru>
2021-08-04 22:59:28 +0300

    "Ээээээээксперименты"

---
+++ src/ru/job4j/types/IntType.java
@@ -0,0 +1,8 @@
+package ru.job4j.types;
+
+import java.util.Scanner;
+
+public class IntType {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/ru/job4j/types/Types.java
@@ -0,0 +1,9 @@
+package ru.job4j.types;
+
+public class Types {
+    public static void main(String[] args) {
+        double d = 121.19;
+        byte b = (byte) d;
+        System.out.println(b);
+    }
+}
