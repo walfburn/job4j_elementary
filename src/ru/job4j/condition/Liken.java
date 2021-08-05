4525af92274228580113f740fb2545c4350392a8
Vladimir <walfburner@mail.ru>
2021-08-05 22:48:59 +0300

    0. Операции сравнения [#235631]

---
+++ src/ru/job4j/condition/Liken.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Liken {
+
+    public static void main(String[] args) {
+        int one = 20;
+        int two = 15;
+
+        boolean result = one > two;
+        System.out.println(result);
+
+        result = one < two;
+        System.out.println(result);
+
+        result = one == two;
+        System.out.println(result);
+    }
+}
