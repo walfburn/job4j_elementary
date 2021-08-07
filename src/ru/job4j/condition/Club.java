7c96b4a557b79a22d4ee5b4cc9c34c28ce1ccb19
Vladimir <walfburner@mail.ru>
2021-08-07 23:39:07 +0300

    4.3.2. Оператор || [#333784]

--- src/ru/job4j/condition/Club.java
+++ src/ru/job4j/condition/Club.java
@@ -2,8 +2,8 @@ package ru.job4j.condition;
 
 public class Club {
 
-    public static void permission(boolean hasMoney, boolean beFried) {
-        if (hasMoney || beFried) {
+    public static void permission(boolean hasMoney, boolean beFriend) {
+        if (hasMoney || beFriend) {
             System.out.println("I can go to the club.");
         } else {
             System.out.println("I can't.");
