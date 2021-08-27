4b817cb5f291b57f6461bf0717e07fb14c8a41b6
Vladimir <walfburner@mail.ru>
2021-08-27 21:34:05 +0300

    5.7. Ипотека [#156315]

---
+++ test/ru/job4j/loop/MortgageTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MortgageTest {
+
+    @Test
+    public void year1() {
+        int amount = 1000;
+        int salary = 1200;
+        double percent = 1;
+        int result = Mortgage.year(amount, salary, percent);
+        int expected = 1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void year2() {
+        int amount = 100;
+        int salary = 120;
+        double percent = 50;
+        int result = Mortgage.year(amount, salary, percent);
+        int expected = 2;
+        Assert.assertEquals(expected, result);
+    }
+}
