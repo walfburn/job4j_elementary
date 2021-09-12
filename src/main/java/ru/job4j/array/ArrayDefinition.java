d7e3bdfdf4c8d472b4b8eb826c6423ae3db2e18e
Vladimir <walfburner@mail.ru>
2021-09-12 23:38:40 +0300

    6.0.2. Заполнение массива.

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер surnames = " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер prices = " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Vladimir";
+        System.out.println(names[0]);
+        names[1] = "Dmitry";
+        System.out.println(names[1]);
+        names[2] = "Daria";
+        System.out.println(names[2]);
+        names[3] = "Alexander";
+        System.out.println(names[3]);
     }
 }
