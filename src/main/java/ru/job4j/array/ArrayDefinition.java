8a0c02106e2b9119c9f8b32ae0c20d13154d1400
Vladimir <walfburner@mail.ru>
2021-09-12 23:05:01 +0300

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,10 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("Размер ages = " + ages.length);
         String[] surnames = new String[100500];
+        System.out.println("Размер surnames = " + surnames.length);
         float[] prices = new float[40];
+        System.out.println("Размер prices = " + prices.length);
     }
 }
