37f2d9172f095e29f55eab137e155cb43c58e9f3
Vladimir <walfburner@mail.ru>
2021-10-13 00:57:15 +0300

    6.4.1.1. Обход массива с последнего элемента [#361619]

---
+++ src/main/java/ru/job4j/array/PrintEvenElements.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class PrintEvenElements {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println(numbers[numbers.length - 1 - i]);
+            }
+        }
+    }
+}
